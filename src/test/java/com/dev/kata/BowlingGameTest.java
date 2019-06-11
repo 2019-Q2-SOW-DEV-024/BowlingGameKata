af596603a8de11b878baffe01dfbf400baacf527
2019-Q2-SOW-DEV-024 <pavanicrazy123@gmail.com>
2019-06-11 15:36:27 +0200

    Refactor:Added methods in test class for rolling miss and spare

--- src/test/java/com/dev/kata/BowlingGameTest.java
+++ src/test/java/com/dev/kata/BowlingGameTest.java
@@ -44,16 +44,15 @@ public class BowlingGameTest {
 	@Test
 	public void testGameScoreWhenRollsAreMiss() {
 		game.roll(5);
-		game.roll(0);
-		game.roll(0);
+		rollMiss();
+		rollMiss();
 		rollPins(17,1);
 		assertEquals(22, game.calculateScore());
 	}
 	
 	@Test
 	public void testGameScoreWhenOneSpare() {
-		game.roll(2);
-		game.roll(8);
+		rollSpare(2);
 		game.roll(7);
 		rollPins(17,2);
 
@@ -63,10 +62,18 @@ public class BowlingGameTest {
 	@Test
 	public void testGameScoreWhenSpareLastRoll() {
 		rollPins(18,2);
-		game.roll(5);
-		game.roll(5);
+		rollSpare(5);
 		game.roll(2);
 
 		assertEquals(48, game.calculateScore());
+	}
+	
+	private void rollSpare(int pins) {
+		game.roll(pins);
+		game.roll(10-pins);
+	}
+
+	private void rollMiss() {
+		game.roll(0);
 	}
 }
