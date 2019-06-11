be8c01037594eefeeed153e471c11a9036ce3208
2019-Q2-SOW-DEV-024 <pavanicrazy123@gmail.com>
2019-06-11 12:33:38 +0200

    validate set up

---
+++ src/test/java/com/dev/kata/BowlingGameTest.java
@@ -0,0 +1,11 @@
+package com.dev.kata;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+public class BowlingGameTest {
+
+	@Test
+	public void validateSetUp() {
+		assertTrue(true);
+	}
+}
