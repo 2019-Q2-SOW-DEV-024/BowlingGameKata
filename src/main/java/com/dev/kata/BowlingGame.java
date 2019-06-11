e2c1da7c9544318c77f8797d9c3275e16ff39976
2019-Q2-SOW-DEV-024 <pavanicrazy123@gmail.com>
2019-06-11 16:31:19 +0200

    Refactor: Extracted logic on check of strike logic as method and using constants,rolling strike as method

--- src/main/java/com/dev/kata/BowlingGame.java
+++ src/main/java/com/dev/kata/BowlingGame.java
@@ -3,22 +3,26 @@ package com.dev.kata;
 public class BowlingGame {
 	private int rolls[]= new int[21];
 	private int currentRoll=0;
-
+	private static final int ZERO= 0;
+	private static final int NO_OF_FRAMES= 10;
+	private static final int MAX_PINS_DOWN= 10;
+	
+	
 	public void roll(int pins) {
 		rolls[currentRoll++]=pins;
 	}
 
 	public int calculateScore() {
-		int score = 0;
-		int rollIndex = 0;
+		int score = ZERO;
+		int rollIndex = ZERO;
 		
-	    for (int frame = 0; frame < 10; frame++) {
-	    	if (rolls[rollIndex] == 10)
+	    for (int frame = ZERO; frame < NO_OF_FRAMES; frame++) {
+	    	if (isStrike(rollIndex))
 	        {
-	    		score += 10 +rolls[rollIndex+1] +rolls[rollIndex+2];
+	    		score += MAX_PINS_DOWN +rolls[rollIndex+1] +rolls[rollIndex+2];
 	          	rollIndex++;
 	        }else if (isSpare(rollIndex)){
-	        	score += 10 + rolls[rollIndex + 2];
+	        	score += MAX_PINS_DOWN + rolls[rollIndex + 2];
 	        	rollIndex += 2;
 	        } else {
 	        	score += rolls[rollIndex] + rolls[rollIndex + 1];
@@ -29,7 +33,11 @@ public class BowlingGame {
 	}
 	
 	private boolean isSpare(int rollIndex) {
-	    return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+	    return rolls[rollIndex] + rolls[rollIndex + 1] == MAX_PINS_DOWN;
+	}
+
+	private boolean isStrike(int rollIndex) {
+	    return rolls[rollIndex] == 10;
 	}
 
 }
